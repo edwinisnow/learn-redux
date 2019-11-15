b364b7aaafdd85619e3547bf1dfbfa38152aef9e
edwinisnow <edwin04@gmail.com>
2019-11-15 00:54:11 +0000

    react-redux-params

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
       {
         isLogged ? <h3>Logged In</h3> : <h3>Logged Out</h3>
