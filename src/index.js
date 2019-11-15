7678ed88cfe559ab9ae681b9db6466f7411dc261
edwinisnow <edwin04@gmail.com>
2019-11-15 00:05:47 +0000

    basic redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// Action 
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+// Reducer 
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return
+    }
+}
+// Store - Global State
+let store = createStore(counter)
+
+store.subscribe(() => console.log(store.getState()))
+
+
+// Dispatch
+store.dispatch(increment())
+store.dispatch(increment())
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
